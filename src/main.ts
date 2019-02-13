2a5c13ee318156cbc72f26d04556ad98ad9fa7e7
jason1466 <jason1466@users.noreply.github.com>
2019-02-13 18:45:57 -0500

    version set to 0.1

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const options = new DocumentBuilder()
     .setTitle('NestJS Test Example App')
     .setDescription('The TeamWiz API description')
-    .setVersion('1.0')
+    .setVersion('0.1')
     .setBasePath('api')
     .addBearerAuth()
     .setSchemes('https')
