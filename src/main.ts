c076f4ed21a2cc979241e2d91db217f83ac9eb2c
jason1466 <jason1466@users.noreply.github.com>
2019-03-02 17:58:24 -0500

    credentials: true

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const appOptions = { cors: true };
+  const appOptions = {
+    cors: true,
+    credentials: true,
+  };
   const app = await NestFactory.create(AppModule, appOptions);
   app.setGlobalPrefix('api');
 
