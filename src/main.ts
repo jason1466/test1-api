90c271c8da946cc8f6f5a037f6769999d776ff64
jason1466 <jason1466@users.noreply.github.com>
2019-03-04 09:43:20 -0500

    appOptions

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,10 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  // const appOptions = {
-  //   cors: true,
-  // };
-  const app = await NestFactory.create(AppModule);
-  // , appOptions);
+  const appOptions = {
+    cors: true,
+  };
+  const app = await NestFactory.create(AppModule, appOptions);
   app.setGlobalPrefix('api');
 
   const options = new DocumentBuilder()
