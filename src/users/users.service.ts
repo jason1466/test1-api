07edd0f05131be6492e0cdf8fd605be24f3693aa
jason1466 <jason1466@users.noreply.github.com>
2019-03-04 10:42:17 -0500

    auth me

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { Model } from 'mongoose';
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
 import { User } from './user.interface';
 import { CreateUserDto } from './create-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -10,18 +10,24 @@ import { UserSchema } from './user.schema';
 export class UsersService {
   constructor(
     @InjectModel('Common') private readonly commonModel: Model<Common>,
+    private readonly http: HttpService,
   ) {}
 
   userModel = this.commonModel.discriminator<User>('User', UserSchema);
+  me: { access_token: string } = { access_token: null };
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser: User = new this.userModel(
-      ...(createUserDto as User),
-      ...({
-        createdAt: new Date().toISOString(),
-        createdBy: 'Jason',
-      } as User),
-    );
+    const createdUser: User = new this.userModel(createUserDto);
+    // ...(createUserDto as User),
+    // ...({
+    //   createdAt: new Date().toISOString(),
+    //   createdBy: 'Jason',
+    // } as User)
+    // );
+    this.me = await this.http
+      .get<any>('/.auth/me')
+      .toPromise()
+      .then(x => x[0]);
     return await createdUser.save();
   }
 
